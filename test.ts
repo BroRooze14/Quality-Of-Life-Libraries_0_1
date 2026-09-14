83451fba70c4504ef79ca95245593cd4a8f5bb24
BroRooze14 <159045075+BroRooze14@users.noreply.github.com>
2026-09-14 22:27:08 +0200

    Adds contents to test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,15 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+input.onButtonPressed(Button.A, function () {
+    gridTools.resetGrid("Grid")
+})
+input.onButtonPressed(Button.B, function () {
+    gridTools.showInTerminal("Grid")
+})
+gridTools.initGrid("Grid")
+gridTools.setDefaultCharacter("Grid", "0")
+gridTools.setGridValue(
+"Grid",
+3,
+1,
+"X"
+)
+serial.writeLine(gridTools.getGridValue("Grid", 0, 0))

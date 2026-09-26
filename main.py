059a756f8ac1d8d872f8947cc47e2323d6a50bce
BroRooze14 <159045075+BroRooze14@users.noreply.github.com>
2026-09-26 11:03:12 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-num_letters = 5
-random_text = ""
-
-for index in range(num_letters):
-    # String.from_char_code is the exact command MakeCode looks for
-    random_text = random_text + String.from_char_code(randint(65, 90))
-
-basic.show_string(random_text)
